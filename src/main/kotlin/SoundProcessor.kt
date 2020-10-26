e431f45a3a0c102fd82801c2292065a9072370f8
Alex Larson <alex.larson@hotmail.com>
2020-10-25 21:35:46 -0500

    Increased the buffer size dynamically to account for various bit depths

--- src/main/kotlin/SoundProcessor.kt
+++ src/main/kotlin/SoundProcessor.kt
@@ -11,34 +11,39 @@ import kotlin.math.sqrt
 
 class SoundProcessor {
 
-    fun startListening() {
-        var line = InputManager().getInputs()[0] as TargetDataLine //TODO: Improve how to pick input; ideally would be like AudioFormat(16000.0f, 16, 2, true, true)
+    //Ideally I'd capture a minimum frequency of 20hz (give or take)
+    //20hz means a sample every 0.05 seconds
+    //48000hz means a sample every 0.000020833333333 seconds
+    //I can infer that a 20 hz signal would require 2500 samples (0.05 / 0.00002 = 2400)
+    //The sample size must be to the power of 2 for FFT; I would need 4096 samples to identify a 20hz signal.
+    //I could cut the number of samples (and thus time) in half by capturing 2048 samples; this would give me a min frequency of 24hz.
+    //24hz is definitely worth the tradeoff considering music seldom goes this low
+
+    val kSampleSize = 2048 //TODO: Calculate dynamically based on the sample rate
+
+    fun startListening() { //TODO: Add condition for prefer performance (pad LF up) vs quality (pad LF down)
+        //TODO: Improve how to pick input
+        var line = InputManager().getInputs()[0] as TargetDataLine
 
         //Open the line
-        line.open()
+        line.open(line.format, line.format.frameSize * kSampleSize)
         line.start()
 
         //Retrieve Audio
-        val buffer = ByteArray(line.bufferSize) //TODO: Explore different size calculations
-        var wave = DoubleArray(line.bufferSize / 2)
+        val buffer = ByteArray(line.bufferSize)
+        var wave: DoubleArray
 
         val audioSamplingThread = Thread(Runnable {
             while (true) {
-                val readBytes = line.read(buffer, 0, line.available())
+                //TODO: Try to make a rolling buffer; for example - if we need 4096 samples to compute, but they only come in 1024 at a time, then insert using FIFO so FFT and compute on more incremental data
+                val readBytes = line.read(buffer, 0, buffer.count())
                 wave = convert(buffer, line.format)
-            }
-        })
 
-        audioSamplingThread.start()
-
-        //Process Audio
-        val fftThread = Thread(Runnable {
-            while (true) {
                 val fftData = process(wave, line.format)
             }
         })
 
-        fftThread.start()
+        audioSamplingThread.start()
     }
 
     //Reference: https://stackoverflow.com/questions/29560491/fourier-transforming-a-byte-array
@@ -69,7 +74,6 @@ class SoundProcessor {
     }
 
     //Reference: https://github.com/wendykierp/JTransforms/issues/4
-    //TODO: Low frequencies are not very accurate.
     private fun process(signal: DoubleArray, format: AudioFormat): DoubleArray {
         //Perform FFT
         val doubleFFT = DoubleFFT_1D(signal.count().toLong())
@@ -85,11 +89,13 @@ class SoundProcessor {
         }
 
         //Determine peak frequency (temporary)
+        //TODO: This is peak frequency frame, NOT PEAK FREQUENCY! Fix.
         val maxValue = result.max()
         val maxIndex = result.indexOfFirst { it == maxValue }
 
-        println(maxIndex * 2)
+        println(maxIndex * format.sampleRate / kSampleSize)
 
         return result
     }
+
 }
